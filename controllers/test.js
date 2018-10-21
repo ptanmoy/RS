d4519c2567af149fecca7a0f319b2eea77ee219a
Tanmoy Palodhi <tanmoypalodhi@outlook.com>
2018-10-21 02:42:14 -0700

    Update test.js

--- controllers/test.js
+++ controllers/test.js
@@ -4,5 +4,5 @@ var Test = require('../models/testmodel');
 exports.test_controller_func = function(req, res) {
     var ID = req.params.uid;
     var BID = req.params.bid;
-    res.send('NOT IMPLEMENTED: TEST CONTROLLER - '+ID+' - '+BID);
+    res.send('ID and Book_ID repectively '+ID+' - '+BID);
 };
