f8ad6912fed52dca52556f5c57d9662a95b2ecab
Tanmoy <tanmoypalodhi@outlook.com>
2018-11-15 02:06:11 -0800

    Working condition file

--- db.js
+++ db.js
@@ -1,17 +1,14 @@
 var pool = require('./pool.js');
 
-exports.getRequirements = funtion() 
-  var retJSON;
   pool.getConnection(function(err, connection){
     connection.query('SELECT * FROM RS_REQUIREMENTS', function(err, results){
       if(err) {
         throw err;
       } else {
-         retJSON = JSON.stringify(results);
+         console.log(JSON.stringify(results));
       }
     });
     connection.release();
-    return retJSON;
   });
 
 
