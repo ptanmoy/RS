98858b6affb6295d98ac1c314959c8f056b035aa
Tanmoy <tanmoypalodhi@outlook.com>
2018-11-17 11:09:33 -0800

    ok

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 var pool = require('./pool.js');
 
 
-let getREQResults = function(callback){
-  pool.getConnection(function(err, connection){
-    connection.query("SELECT * FROM RS_REQUIREMENTS WHERE REQUIREMENTS_STATUS='OPEN'", function(err, results){
-      if(err) {
+let getREQResults = function (callback) {
+  pool.getConnection(function (err, connection) {
+    connection.query("SELECT * FROM RS_REQUIREMENTS WHERE REQUIREMENTS_STATUS='OPEN'", function (err, results) {
+      if (err) {
         return err;
       } else {
         connection.release();
         callback(results);
       }
-    });
-    
-  }); 
+    });//
+
+  });
 }
 
 module.exports.getREQResults = getREQResults;
